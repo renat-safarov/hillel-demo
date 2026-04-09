c627f5425e3ad523f5f241cd3c7b5b495caaac4b
Renat Safarov <safarovrenat1995@gmail.com>
2026-04-09 21:09:09 +0300

    Update messages

--- src/DemoApp.java
+++ src/DemoApp.java
@@ -1,6 +1,6 @@
 public class DemoApp {
     public static void main(String[] args) {
-        System.out.println("Add feature");
-        System.out.println("Update feature");
+        System.out.println("Add feature!");
+        System.out.println("Update feature!");
     }
 }
